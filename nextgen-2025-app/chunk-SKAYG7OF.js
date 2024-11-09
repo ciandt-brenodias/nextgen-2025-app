5eb7b45cbe4d6bb5a98be4cdf26d412af7e792f9
ciandt-wcaetano <ciandt-wcaetano@users.noreply.github.com>
2024-11-09 01:58:48 +0000

    deploy: aa3b3b6615cdc22dc0de159f3b0f94dc26918a00

---
+++ nextgen-2025-app/chunk-SKAYG7OF.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-FL4TJ244.js";import"./chunk-PZN5S6BE.js";import"./chunk-OSECSOUH.js";export{a as MentoriasModule};
--- nextgen-2025-app/chunk-VAS3E2SX.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-EKXMCQKO.js";import"./chunk-A5NPB3FU.js";import"./chunk-OSECSOUH.js";export{a as MentoriasModule};
